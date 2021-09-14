f0f2b7dd5f2d298e065ab58acfaa79656e62c297
Swati <swatimaurya9581@gmail.com>
2021-09-14 05:29:30 -0700

    Initial commit of BajajCapital

---
+++ script.js
@@ -0,0 +1,191 @@
+//submit method
+let loader1, loader2, loader3, loader4;
+
+function submit() {
+    console.log("Responsiveness : " + loader1 +
+        "\nCompetency in advising right solutions : " + loader2 +
+        "\nTrust worthiness : " + loader3 +
+        "\nCustomer service and support : " + loader4);
+}
+let green = "green";
+let yellow = "yellow";
+//loader 4
+function function4A() {
+    document.getElementById("loader4A").style.backgroundColor = "rgb(234, 238, 9)";
+    loader4 = document.getElementById("loader4A").innerHTML;
+    document.getElementById("loader4B").style.backgroundColor = "grey";
+    document.getElementById("loader4C").style.backgroundColor = "grey";
+    document.getElementById("loader4D").style.backgroundColor = "grey";
+    document.getElementById("loader4E").style.backgroundColor = "grey";
+}
+
+function function4B() {
+    document.getElementById("loader4A").style.backgroundColor = "rgb(234, 238, 9)";
+    document.getElementById("loader4B").style.backgroundColor = "rgb(234, 238, 9)";
+    loader4 = document.getElementById("loader4B").innerHTML;
+    document.getElementById("loader4C").style.backgroundColor = "grey";
+    document.getElementById("loader4D").style.backgroundColor = "grey";
+    document.getElementById("loader4E").style.backgroundColor = "grey";
+}
+
+function function4C() {
+    document.getElementById("loader4A").style.backgroundColor = "rgb(234, 238, 9)";
+    document.getElementById("loader4B").style.backgroundColor = "rgb(234, 238, 9)";
+    document.getElementById("loader4C").style.backgroundColor = "rgb(234, 238, 9)";
+    loader4 = document.getElementById("loader4C").innerHTML;
+    document.getElementById("loader4D").style.backgroundColor = "grey";
+    document.getElementById("loader4E").style.backgroundColor = "grey";
+}
+
+function function4D() {
+    document.getElementById("loader4A").style.backgroundColor = "rgb(234, 238, 9)";
+    document.getElementById("loader4B").style.backgroundColor = "rgb(234, 238, 9)";
+    document.getElementById("loader4C").style.backgroundColor = "rgb(234, 238, 9)";
+    document.getElementById("loader4D").style.backgroundColor = "rgb(234, 238, 9)";
+    loader4 = document.getElementById("loader4D").innerHTML;
+    document.getElementById("loader4E").style.backgroundColor = "grey";
+}
+
+function function4E() {
+    document.getElementById("loader4A").style.backgroundColor = "rgb(234, 238, 9)";
+    document.getElementById("loader4B").style.backgroundColor = "rgb(234, 238, 9)";
+    document.getElementById("loader4C").style.backgroundColor = "rgb(234, 238, 9)";
+    document.getElementById("loader4D").style.backgroundColor = "rgb(234, 238, 9)";
+    document.getElementById("loader4E").style.backgroundColor = "rgb(234, 238, 9)";
+    loader4 = document.getElementById("loader4E").innerHTML;
+}
+//loader 3
+function function3A() {
+    document.getElementById("loader3A").style.backgroundColor = "#5fe25f";
+    loader3 = document.getElementById("loader3A").innerHTML;
+    document.getElementById("loader3B").style.backgroundColor = "grey";
+    document.getElementById("loader3C").style.backgroundColor = "grey";
+    document.getElementById("loader3D").style.backgroundColor = "grey";
+    document.getElementById("loader3E").style.backgroundColor = "grey";
+}
+
+function function3B() {
+    document.getElementById("loader3A").style.backgroundColor = "#5fe25f";
+    document.getElementById("loader3B").style.backgroundColor = "#5fe25f";
+    loader3 = document.getElementById("loader3B").innerHTML;
+    document.getElementById("loader3C").style.backgroundColor = "grey";
+    document.getElementById("loader3D").style.backgroundColor = "grey";
+    document.getElementById("loader3E").style.backgroundColor = "grey";
+}
+
+function function3C() {
+    document.getElementById("loader3A").style.backgroundColor = "#5fe25f";
+    document.getElementById("loader3B").style.backgroundColor = "#5fe25f";
+    document.getElementById("loader3C").style.backgroundColor = "#5fe25f";
+    loader3 = document.getElementById("loader3C").innerHTML;
+    document.getElementById("loader3D").style.backgroundColor = "grey";
+    document.getElementById("loader3E").style.backgroundColor = "grey";
+}
+
+function function3D() {
+    document.getElementById("loader3A").style.backgroundColor = "#5fe25f";
+    document.getElementById("loader3B").style.backgroundColor = "#5fe25f";
+    document.getElementById("loader3C").style.backgroundColor = "#5fe25f";
+    document.getElementById("loader3D").style.backgroundColor = "#5fe25f";
+    loader3 = document.getElementById("loader3D").innerHTML;
+    document.getElementById("loader3E").style.backgroundColor = "grey";
+}
+
+function function3E() {
+    document.getElementById("loader3A").style.backgroundColor = "#5fe25f";
+    document.getElementById("loader3B").style.backgroundColor = "#5fe25f";
+    document.getElementById("loader3C").style.backgroundColor = "#5fe25f";
+    document.getElementById("loader3D").style.backgroundColor = "#5fe25f";
+    document.getElementById("loader3E").style.backgroundColor = "#5fe25f";
+    loader3 = document.getElementById("loader3E").innerHTML;
+}
+//loader 2
+function function2A() {
+    document.getElementById("loader2A").style.backgroundColor = yellow;
+    loader2 = document.getElementById("loader2A").innerHTML;
+    document.getElementById("loader2B").style.backgroundColor = "grey";
+    document.getElementById("loader2C").style.backgroundColor = "grey";
+    document.getElementById("loader2D").style.backgroundColor = "grey";
+    document.getElementById("loader2E").style.backgroundColor = "grey";
+}
+
+function function2B() {
+    document.getElementById("loader2A").style.backgroundColor = yellow;
+    document.getElementById("loader2B").style.backgroundColor = yellow;
+    loader2 = document.getElementById("loader2B").innerHTML;
+    document.getElementById("loader2C").style.backgroundColor = "grey";
+    document.getElementById("loader2D").style.backgroundColor = "grey";
+    document.getElementById("loader2E").style.backgroundColor = "grey";
+}
+
+function function2C() {
+    document.getElementById("loader2A").style.backgroundColor = yellow;
+    document.getElementById("loader2B").style.backgroundColor = yellow;
+    document.getElementById("loader2C").style.backgroundColor = yellow;
+    loader2 = document.getElementById("loader2C").innerHTML;
+    document.getElementById("loader2D").style.backgroundColor = "grey";
+    document.getElementById("loader2E").style.backgroundColor = "grey";
+}
+
+function function2D() {
+    document.getElementById("loader2A").style.backgroundColor = yellow;
+    document.getElementById("loader2B").style.backgroundColor = yellow;
+    document.getElementById("loader2C").style.backgroundColor = yellow;
+    document.getElementById("loader2D").style.backgroundColor = yellow;
+    loader2 = document.getElementById("loader2D").innerHTML;
+    document.getElementById("loader2E").style.backgroundColor = "grey";
+}
+
+function function2E() {
+    document.getElementById("loader2A").style.backgroundColor = yellow;
+    document.getElementById("loader2B").style.backgroundColor = yellow;
+    document.getElementById("loader2C").style.backgroundColor = yellow;
+    document.getElementById("loader2D").style.backgroundColor = yellow;
+    document.getElementById("loader2E").style.backgroundColor = yellow;
+    loader2 = document.getElementById("loader2E").innerHTML;
+}
+//loader 1
+function function1A() {
+    document.getElementById("loader1A").style.backgroundColor = green;
+    loader1 = document.getElementById("loader1A").innerHTML;
+    document.getElementById("loader1B").style.backgroundColor = "grey";
+    document.getElementById("loader1C").style.backgroundColor = "grey";
+    document.getElementById("loader1D").style.backgroundColor = "grey";
+    document.getElementById("loader1E").style.backgroundColor = "grey";
+}
+
+function function1B() {
+    document.getElementById("loader1A").style.backgroundColor = green;
+    document.getElementById("loader1B").style.backgroundColor = green;
+    loader1 = document.getElementById("loader1B").innerHTML;
+    document.getElementById("loader1C").style.backgroundColor = "grey";
+    document.getElementById("loader1D").style.backgroundColor = "grey";
+    document.getElementById("loader1E").style.backgroundColor = "grey";
+}
+
+function function1C() {
+    document.getElementById("loader1A").style.backgroundColor = green;
+    document.getElementById("loader1B").style.backgroundColor = green;
+    document.getElementById("loader1C").style.backgroundColor = green;
+    loader1 = document.getElementById("loader1C").innerHTML;
+    document.getElementById("loader1D").style.backgroundColor = "grey";
+    document.getElementById("loader1E").style.backgroundColor = "grey";
+}
+
+function function1D() {
+    document.getElementById("loader1A").style.backgroundColor = green;
+    document.getElementById("loader1B").style.backgroundColor = green;
+    document.getElementById("loader1C").style.backgroundColor = green;
+    document.getElementById("loader1D").style.backgroundColor = green;
+    loader1 = document.getElementById("loader1D").innerHTML;
+    document.getElementById("loader1E").style.backgroundColor = "grey";
+}
+
+function function1E() {
+    document.getElementById("loader1A").style.backgroundColor = green;
+    document.getElementById("loader1B").style.backgroundColor = green;
+    document.getElementById("loader1C").style.backgroundColor = green;
+    document.getElementById("loader1D").style.backgroundColor = green;
+    document.getElementById("loader1E").style.backgroundColor = green;
+    loader1 = document.getElementById("loader1E").innerHTML;
+}
